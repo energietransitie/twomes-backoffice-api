f13dbb6ec6fe2a2d4e732b9ececd344f4b73b1e4
Kevin Jansen <kevin@vjkd.nl>
2021-05-20 21:46:58 +0200

    Update user.py
    Added myself as admin to user.py so that I have admin privileges for the devices as discussed earlier today.

--- src/user.py
+++ src/user.py
@@ -8,7 +8,8 @@ from auth import (
 
 admins = (
     (1, 'arjan', '$2b$12$BAkuzJVXNwgeiU2vBL5b0eBPDm777eI0bvnjqKmlpDNGBr/hdGnjq'),
-    (2, 'Marco', '$2b$12$mP7XyiffPKDGJM05xGPrBuaE8yoLVjabaXj1.UYaxFYdd3bpLzUwu')
+    (2, 'Marco', '$2b$12$mP7XyiffPKDGJM05xGPrBuaE8yoLVjabaXj1.UYaxFYdd3bpLzUwu'),
+    (3, 'Kevin', '$2b$12$9iG1PXmGN9BJHyypW6rMgOT2wHx/zrJhg/rZT0/GgxTnwimnl1CBC')
     # Add extra admin tuples, created with 'create_admin()', here
 )
 admins_name = {a[0]: a[1] for a in admins}
