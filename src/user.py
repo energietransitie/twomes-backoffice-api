bdaa85a5fed3ef48ed50017c40cae34a56ba6866
Henri ter Hofte <h.ter.hofte@windesheim.nl>
2022-06-23 21:26:20 +0200

    restrict users for deployment to production

--- src/user.py
+++ src/user.py
@@ -7,9 +7,7 @@ from auth import (
 )
 
 admins = (
-    (4, 'henrith', '$2b$12$tKBneY7fbLY4aMocM6OdOuhSoccx5o0dL/hJr.9ZqzraX1QetqF2O'),
-    (10, 'seceng', '$2b$12$Cz1pcfw470GO7aUv/C/Ly.P0Ru7fGLcUBTj.1zSZdE0IwPy2ZSwz6'),
-    (11, 'nick', '$2b$12$f8iLYhz0Vt4n0TKpG.bw2.MERr5uBlFShzzUztLZjb.TfQBmP2vLy')
+    (4, 'henrith', '$2b$12$tKBneY7fbLY4aMocM6OdOuhSoccx5o0dL/hJr.9ZqzraX1QetqF2O')
     # Add extra admin tuples, created with 'create_admin()', here
 )
 admins_name = {a[0]: a[1] for a in admins}
