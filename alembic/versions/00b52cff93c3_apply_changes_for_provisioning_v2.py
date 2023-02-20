8a2923a82b5e53b2a1797b7b2f262ff6bb4e62cf
n-vr <43145188+n-vr@users.noreply.github.com>
2023-02-20 14:41:00 +0100

    Remove building characteristics
    We doubt if these characteristics will survive
    privacy (re-identification) check, so remove them.
    
    Trello: https://trello.com/c/vNpVum0G

--- alembic/versions/00b52cff93c3_apply_changes_for_provisioning_v2.py
+++ alembic/versions/00b52cff93c3_apply_changes_for_provisioning_v2.py
@@ -43,9 +43,24 @@ def upgrade():
                             comment='URL to information about a device type'))
     # A device_type's display name is language dependent, so we remove it from the DB.
     op.drop_column('device_type', 'display_name')
+    
+    # Remove building characteristics
+    op.drop_column('building', 'yr_built')
+    op.drop_column('building', 'type')
+    op.drop_column('building', 'floor_area')
+    op.drop_column('building', 'heat_loss_area')
+    op.drop_column('building', 'energy_label')
+    op.drop_column('building', 'energy_index')
 
 
 def downgrade():
+    op.add_column('building', sa.Column('yr_built', sa.Text(), nullable=True, comment='Year built ("oorspronkelijk bouwjaar")'))
+    op.add_column('building', sa.Column('type', sa.Text(), nullable=True, comment='House type ("woningtype")'))
+    op.add_column('building', sa.Column('floor_area', sa.Integer(), nullable=True, comment='Floor area ("gebruiksoppervlakte"), as defined in NEN NTA 8800'))
+    op.add_column('building', sa.Column('heat_loss_area', sa.Integer(), nullable=True, comment='Heat loss area ("verliesoppervlakte"), as defined in NEN NTA 8800'))
+    op.add_column('building', sa.Column('energy_label', sa.Text(), nullable=True, comment='Energy label, as defined in NEN NTA 8800'))
+    op.add_column('building', sa.Column('energy_index', sa.Float(), nullable=True, comment='Energy index, as defined in NEN NTA 8800'))
+    
     op.add_column('device_type', sa.Column('display_name', sa.Text(), nullable=True,
                                            comment='Name to show in user interfaces'))
     op.drop_column('device_type', 'info_url')
