d1257a1f699307d91e5d4c0f7f2a85300b36af5b
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2024-03-18 11:02:11 +0100

    Feat: Use cronjob format for uploadschedule

--- twomes/shoppingitem/shoppingitem.go
+++ twomes/shoppingitem/shoppingitem.go
@@ -2,7 +2,7 @@ package shoppingitem
 
 import "github.com/energietransitie/twomes-backoffice-api/twomes/shoppingitemtype"
 
-// Interface to allow device, cloudfeed and energyquery in one action.
+// Interface to allow device, cloudfeed and energyquery ID in one table. we use type to rely on selecting the right table
 type ActionModel interface{}
 
 // An item can be a device, cloudfeed or energyquery
--- twomes/uploadschedule/uploadschedule.go
+++ twomes/uploadschedule/uploadschedule.go
@@ -1,8 +1,6 @@
 package uploadschedule
 
 import (
-	"time"
-
 	"github.com/energietransitie/twomes-backoffice-api/twomes/shoppingitem"
 )
 
@@ -10,5 +8,5 @@ import (
 type UploadSchedule struct {
 	ID       uint                      `json:"id"`
 	Item     shoppingitem.ShoppingItem `json:"item"`
-	Schedule []time.Time               `json:"schedule"` //Schedule in seconds. So for every 10, 30, 60 minutes. you'd need to set it to [60, 1800, 3600]
+	Schedule []string                  `json:"schedule"` //Schedule in cronjob format, can be multiple
 }
