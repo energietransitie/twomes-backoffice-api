3481d3c5dca40bf3edae34516baf3a8822fff3ec
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2024-03-18 11:17:11 +0100

    Feat: Dependency field in ShoppingList for partial list

--- twomes/shoppinglist/shoppinglist.go
+++ twomes/shoppinglist/shoppinglist.go
@@ -6,6 +6,7 @@ import (
 
 // A shoppinglist is a collection of shoppingitems that are part of a campaign.
 type ShoppingList struct {
-	ID    uint                        `json:"id"`
-	Items []shoppingitem.ShoppingItem `json:"items,omitempty"`
+	ID           uint                           `json:"id"`
+	Items        []shoppingitem.ShoppingItem    `json:"items,omitempty"`
+	Dependencies [][2]shoppingitem.ShoppingItem `json:"dependencies"` //Example: [[1,2],[3,2]], 1 and 3 should be done before 2
 }
