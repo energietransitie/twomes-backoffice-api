375c4aa8b85eaa72229bc628b81acb4e30a55636
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2024-04-25 15:51:22 +0200

    Refactor Remove ISO8601 from time

--- needforheat/time.go
+++ needforheat/time.go
@@ -5,31 +5,19 @@ import (
 	"time"
 )
 
-// A Time is a wrapper for time.Time that can be unmarshalled from ISO8601 or unix seconds in JSON.
+// A Time is a wrapper for time.Time that can be unmarshalled into unix seconds in JSON.
 type Time time.Time
 
 func (t Time) MarshalJSON() ([]byte, error) {
 	return json.Marshal(time.Time(t))
 }
 
-// Unmarshal JSON to Time. Can be ISO8601 or unix seconds.
+// Unmarshal JSON to Time. Unix seconds.
 func (t *Time) UnmarshalJSON(b []byte) error {
-	isoTime, err := UnmarshalISO8601(b)
-	if err == nil {
-		*t = Time(isoTime)
-		return nil
-	}
-
 	unixTime, err := UnmarshalUnix(b)
 
 	*t = Time(unixTime)
 	return err
-}
-
-func UnmarshalISO8601(b []byte) (time.Time, error) {
-	var time time.Time
-	err := json.Unmarshal(b, &time)
-	return time, err
 }
 
 func UnmarshalUnix(b []byte) (time.Time, error) {
